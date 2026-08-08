854d456bc70d3f13f73b407dd0c62910fd3b0644
Eduardo <eduoliv.dev@gmail.com>
2026-08-08 13:50:05 -0300

    feat: interface de cadastro integrada

---
+++ src/login/loginScript.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+    const app = document.getElementById("app");
+
+    loader.style.opacity = "0";
+
+    setTimeout(() => {
+
+        loader.remove();
+        app.classList.add("show");
+
+    }, 500);
+
+});
+
+
+let LoginBtn = document.getElementById("Login-btn")
+
+LoginBtn.addEventListener("click", function () {
+
+
+    let nome = document.getElementById("name")
+    let email = document.getElementById("email")
+    let Telefone = document.getElementById("Telefone")
+    let password = document.getElementById("password")
+
+    nome.value = ""
+    email.value = ""
+    Telefone.value = ""
+    password.value = ""
+
+})
